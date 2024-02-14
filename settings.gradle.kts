06bb6b6598bad89db197b0db0791b328de70910f
Luxus <44326048+Luxusio@users.noreply.github.com>
2024-02-14 23:00:34 +0900

    build: ci.yml, cd.yml

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,4 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
 }
 rootProject.name = "Java-Dependency-Analyzer"
-include("SampleProject")
 include("SampleJar")
-include("SampleProject")
