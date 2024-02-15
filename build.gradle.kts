e1a99086f60c25bebc2f2d97bd323a4386bea947
Luxus <44326048+Luxusio@users.noreply.github.com>
2024-02-15 13:15:43 +0900

    build: fix cannot run program (#1)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.9.22"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
 group = "io.luxus"
@@ -7,9 +8,11 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    gradlePluginPortal()
 }
 
 dependencies {
+    implementation("gradle.plugin.com.github.johnrengelman:shadow:7.1.2")
     implementation("org.ow2.asm:asm:9.5")
     implementation("org.ow2.asm:asm-tree:9.5")
     implementation("org.reflections:reflections:0.9.12")
@@ -19,6 +22,15 @@ dependencies {
     testImplementation("io.kotest:kotest-assertions-core:5.6.2")
 }
 
+tasks {
+    withType<Jar> {
+        manifest {
+            attributes["Main-Class"] = "io.luxus.jda.MainKt"
+            attributes["Implementation-Title"] = rootProject.name
+            attributes["Implementation-Version"] = rootProject.version
+        }
+    }
+}
 tasks.test {
     useJUnitPlatform()
 }
